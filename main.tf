296fc768936d64b56159687cfd89c6e60a5ebe07
Alexis Sotto Maior <alexis.maior@planejamento.gov.br>
2021-07-21 18:28:49 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "applications" {
+
+  for_each = var.charts
+
+  name = each.key
+
+  repository = each.value.repo
+  chart      = each.value.chart
+  namespace  = each.value.namespace
+
+  dynamic "set" {
+    for_each = each.value.values
+    content {
+      name  = set.value.name
+      value = set.value.value
+    }
+  }
+}
